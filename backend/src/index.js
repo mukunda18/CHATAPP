8a2769c61324d69e903a437ed3019f13018d2505
Mukunda Chaudhary <109866836+mukunda18@users.noreply.github.com>
2025-11-30 08:54:16 +0545

    index.js updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 import connectDB from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js";
-import cookieParser from "cookie-parser";
-import { app, server } from "./lib/socket.js";
+import cookieParser from "cookie-parser"
+import { app, server } from "./lib/socket.js"
 import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
+
 
 if (process.env.NODE_ENV === "development") {
   app.use(
@@ -28,11 +29,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
 }
 
 connectDB().then(() => {
