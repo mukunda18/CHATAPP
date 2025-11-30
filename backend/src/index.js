e6c502e61affefe763c527766d997877c753ca02
Mukunda Chaudhary <109866836+mukunda18@users.noreply.github.com>
2025-11-30 08:47:38 +0545

    ...

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,12 +28,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
 }
 
 connectDB().then(() => {
